83bb477cd52eeda0c9a7bf076403cd23094a74a7
Dalya Sohl <dalyasohl@hotmail.com>
2022-01-25 22:28:52 -0500

    finished

--- index.js
+++ index.js
@@ -2,15 +2,17 @@ let weeklyExpenses = 0
 let monthlyExpenses = 0
 let annualExpenses = 0
 
-let answer = parseFloat(window.prompt(weeklyExpenseQuestions[0]))
+for(let x = 0; x < weeklyExpenseQuestions.length; x++) {
+let answer = parseFloat(window.prompt(weeklyExpenseQuestions[x]))
 weeklyExpenses = weeklyExpenses + answer
-
-let weeklyExpenses = 0
-let monthlyExpenses = 0
-let annualExpenses = 0
-
-for(){
-    let answer = parseFloat(window.prompt(weeklyExpenseQuestions[0]))
-    weeklyExpenses = weeklyExpenses + answer
 }
 
+for(let x = 0; x < monthlyExpenseQuestions.length; x++) {
+    let answer = parseFloat(window.prompt(monthlyExpenseQuestions[x]))
+    monthlyExpenses = monthlyExpenses + answer
+}
+
+for(let x = 0; x < annualExpenseQuestions.length; x++) {
+    let answer = parseFloat(window.prompt(annualExpenseQuestions[x]))
+    annualExpenses = annualExpenses + answer
+}
