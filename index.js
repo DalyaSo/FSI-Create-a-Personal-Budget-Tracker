fe69a426633a9d2e65536a210b417a40de1bffc5
Dalya Sohl <dalyasohl@hotmail.com>
2022-01-25 21:29:02 -0500

    my repository

--- index.js
+++ index.js
@@ -2,4 +2,15 @@ let weeklyExpenses = 0
 let monthlyExpenses = 0
 let annualExpenses = 0
 
-// Your Code Here
+let answer = parseFloat(window.prompt(weeklyExpenseQuestions[0]))
+weeklyExpenses = weeklyExpenses + answer
+
+let weeklyExpenses = 0
+let monthlyExpenses = 0
+let annualExpenses = 0
+
+for(){
+    let answer = parseFloat(window.prompt(weeklyExpenseQuestions[0]))
+    weeklyExpenses = weeklyExpenses + answer
+}
+
